8491fbd97ca6e28ffd9cecb5de5753dd1624b726
doctor05 <doctorbyron05@.com>
2016-12-26 19:59:14 +0200

    add new forms

--- app/controllers/tsteps_controller.rb
+++ app/controllers/tsteps_controller.rb
@@ -1,32 +1,9 @@
 class TstepsController < ApplicationController
 
-  def  index
-    @tsteps = Tstep.all
-  end
-
-  def show
-    @tstep = Tstep.find(params[:id])
-  end
-
-  def new
-    @tstep = Tstep.new
-
-  end
-
   def create
     @tcase = Tcase.find(params[:tcase_id])
     @tstep = @tcase.tsteps.create(tstep_params)
     redirect_to tcases_path
-
-  end
-
-  def edit
-    @tstep = Tstep.find(params[:id])
-
-  end
-
-  def update
-    @tstep = Tstep.find(params[:id])
 
   end
 
